69ac97a798831d85fe867af7b798838f9b5e40c6
TurpanWest <168362914+TurpanWest@users.noreply.github.com>
2025-05-04 23:50:41 +0800

    Update ships.js

--- ships.js
+++ ships.js
@@ -1,10 +1,16 @@
 const DEFAULT_SHIPS = [
   { name: "岛风", type: "DD" },
-  { name: "大凤", type: "CV" },
-  { name: "伊19", type: "SS" },
-  { name: "蒙大拿", type: "BB" },
-  { name: "吉野B", type: "CA" },
-  { name: "Z-52", type: "DD" },
+  { name: "春云", type: "DD" },
+  { name: "疾风", type: "DD" },
+  { name: "大和", type: "BB" },
+  { name: "敷岛", type: "BB" },
+  { name: "丰后", type: "BB" },
+  { name: "吉野", type: "CA" },
+  { name: "北上", type: "CA" },
+  { name: "藏王", type: "CA" },
+  { name: "淀", type: "CA" },
+  { name: "白龙", type: "CV" },
   { name: "信浓", type: "CV" },
   { name: "U-2501", type: "SS" }
+  { name: "U-4501", type: "SS" }
 ];
