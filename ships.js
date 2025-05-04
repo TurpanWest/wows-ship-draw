b456ae9a1c642a39d67157580cd08602530f5a4e
TurpanWest <168362914+TurpanWest@users.noreply.github.com>
2025-05-04 23:20:57 +0800

    Create ships.js

---
+++ ships.js
@@ -0,0 +1,10 @@
+const DEFAULT_SHIPS = [
+  { name: "岛风", type: "DD" },
+  { name: "大凤", type: "CV" },
+  { name: "伊19", type: "SS" },
+  { name: "蒙大拿", type: "BB" },
+  { name: "吉野B", type: "CA" },
+  { name: "Z-52", type: "DD" },
+  { name: "信浓", type: "CV" },
+  { name: "U-2501", type: "SS" }
+];
